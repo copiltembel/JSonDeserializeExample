22d531f076af4ab413a91d56fe69dc9536ae1a82
Tudor Zaharia <tudor.zaharia@servicehunter.ch>
2018-03-20 20:35:54 +0100

    Example

--- JSonDeserializeExample/Program.cs
+++ JSonDeserializeExample/Program.cs
@@ -1,15 +1,52 @@
-﻿using System;
+﻿using DeserializeDb.DataAccess.DataSet;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace JSonDeserializeExample
+namespace ConsoleApp1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Test();
+        }
+
+        public static void Test()
+        {
+            var profiles = GetProfiles();
+            JsonSerializerSettings settings = new JsonSerializerSettings()
+            {
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+                PreserveReferencesHandling = Newtonsoft.Json.PreserveReferencesHandling.All
+            };
+            var serializedProfiles = JsonConvert.SerializeObject(profiles, Formatting.None,
+                        settings);
+            var deserializedProfiles = JsonConvert.DeserializeObject<List<Profile>>(serializedProfiles, settings);
+        }
+
+        public static List<Profile> GetProfiles()
+        {
+            var profile = new Profile()
+            {
+                Birthday = DateTime.Now,
+                CanBeRecommended = true,
+                ProfileId = Guid.NewGuid()
+            };
+            var locations = new List<Location>();
+            locations.Add(new Location()
+            {
+                Address = "Here",
+                City = "There",
+                LocationId = Guid.NewGuid(),
+                Profile = profile
+            });
+
+            return new List<Profile>() { profile };
         }
     }
 }
--- JSonDeserializeExample/Properties/AssemblyInfo.cs
+++ JSonDeserializeExample/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("JSonDeserializeExample")]
+[assembly: AssemblyTitle("ConsoleApp1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("JSonDeserializeExample")]
+[assembly: AssemblyProduct("ConsoleApp1")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("06b36360-1924-40e0-8579-fbf4f99f70ee")]
+[assembly: Guid("cbadc268-aa99-44e7-85c7-8763a7054280")]
 
 // Version information for an assembly consists of the following four values:
 //
